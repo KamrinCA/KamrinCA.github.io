bf0e1b81d1139027463bb40db0dcba1b93045b17
Kamrin Aubin <kamrinaubin@outlook.com>
2022-01-28 17:48:53 -0500

    added all ICE-3 additions to live site

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,9 @@
 // IIFE -- Immediately Invoked Function Expression
 // AKA anonymous self-executing function
+"use strict";
 (function()
 {
-    function DisplayHomePage()
+    function DisplayHome()
     {
         console.log("Home Page");
 
@@ -34,9 +35,9 @@
     // Step 4 - perform insertion / deletion
 
     // Example of insert after (append)
-    MainContent.appendChild(MainParagraph);
-    Article.innerHTML = ArticleParagraph;
-    DocumentBody.appendChild(Article);
+    // MainContent.appendChild(MainParagraph);
+    // Article.innerHTML = ArticleParagraph;
+    // DocumentBody.appendChild(Article);
 
 
     // Example of insert before
@@ -48,24 +49,86 @@
 
     // ES6 and HTML5 => Template Strings => "Super Strings"
 
+    function DisplayAboutPage()
+    {
+        console.log("About Us Page");
+    }
+
     function DisplayProjectsPage()
     {
-        console.log("Projects Page");
+        console.log(" Our Projects Page");
     }
 
     function DisplayServicesPage()
     {
-        console.log("Services Page");
-    }
-
-    function DisplayAboutPage()
-    {
-        console.log("About Page");
+        console.log("Our Services Page");
     }
 
     function DisplayContactPage()
     {
-        console.log("Contact Page");
+        console.log("Contact Us Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        // localStorage.setItem("Test1", "This is my test");
+        // console.log(localStorage.getItem("Test1"));
+        // localStorage.removeItem("Test1");
+        // console.log("length: " + localStorage.length);
+
+        sendButton.addEventListener("click", function(event)
+        {
+            if (subscribeCheckbox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = ""; // data container -> add deserialized data from the localStorage
+
+            let keys = Object.keys(localStorage); // returns a string array of keys
+
+            let index = 1; // counts how many keys
+            
+            // For every key in the keys array (collection), loop
+            for (const key of keys) 
+            {
+                let contactData = localStorage.getItem(key); // get localStorage data value related to the key
+
+                let contact = new Contact(); // create a new empty contact object
+                contact.deserialize(contactData);
+
+                // inject a repeatable row into the contactList
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>
+                `;
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
     }
 
     // Named function option
@@ -77,7 +140,11 @@
         {
             // Finite State Machine
             case "Home":
-                DisplayHomePage();
+                DisplayHome();
+                break;
+
+            case "About Us":
+                DisplayAboutPage();
                 break;
 
             case "Our Projects":
@@ -88,8 +155,8 @@
                 DisplayServicesPage();
                 break;
 
-            case "About Us":
-                DisplayAboutPage();
+            case "Contact-List":
+                DisplayContactListPage();
                 break;
 
             case "Contact Us":
